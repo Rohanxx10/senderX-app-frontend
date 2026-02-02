30ce8664491061567b188a76e58aef2abb5a40a5
Rohanxx10 <rohanrai7217@gmail.com>
2026-02-02 17:34:41 +0530

    Change baseURL for Axios API instance

--- src/service/Axios.js
+++ src/service/Axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const api = axios.create({
 
-  baseURL: `https://superdrop-xychcyn4.b4a.run/api/v1`
+  baseURL: `https://senderbackend-qkd3qokr.b4a.run/`
 });
 
 export const fileApi= axios.create({

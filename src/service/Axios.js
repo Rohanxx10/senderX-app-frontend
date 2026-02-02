301e4651b20bf3e586974bd15a1e1812e473bed0
Rohanxx10 <rohanrai7217@gmail.com>
2026-02-03 00:14:58 +0530

    Update Axios.js

--- src/service/Axios.js
+++ src/service/Axios.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-
+const export url="https://senderbackend-35quh1uz.b4a.run"
 const api = axios.create({
 
-  baseURL: `https://senderbackend-35quh1uz.b4a.run/api/v1`
+  baseURL: `${url}/api/v1`
 });
 
 export const fileApi= axios.create({
-  baseURL: `${import.meta.env.VITE_APP_API_URL}/api/v1/api/files`
+  baseURL: `${url}/api/v1/api/files`
 });
 
 export default api;
